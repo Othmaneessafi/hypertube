1565da76fe0eef305aecea4a0501d550354402fa
OthmaneOthy <44867969+OthmaneOthy@users.noreply.github.com>
2021-04-04 18:26:02 +0100

    profile movie almost done

--- client/src/components/profileMovie/index.js
+++ client/src/components/profileMovie/index.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import './index.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import Grid from '@material-ui/core/Grid';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import YouTubeIcon from '@material-ui/icons/YouTube';
-import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
-import Button from '@material-ui/core/Button';
-import Comment from './comment'
+import React from "react";
+import "./index.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import Grid from "@material-ui/core/Grid";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import Button from "@material-ui/core/Button";
+import Comment from "./comment";
 import Modal from "../shared/modal";
 import { Redirect } from "react-router-dom";
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import { LinearProgress } from '@material-ui/core';
-import CircularProgress from '@material-ui/core/CircularProgress';
-
-const useStyles = makeStyles(theme => ({
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { LinearProgress } from "@material-ui/core";
+import StarIcon from '@material-ui/icons/Star';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import CircularProgress from "@material-ui/core/CircularProgress";
+const useStyles = makeStyles((theme) => ({
   gridList: {
-    flexWrap: 'nowrap',
+    flexWrap: "nowrap",
     height: 320,
   },
   title: {
@@ -29,62 +30,70 @@ const useStyles = makeStyles(theme => ({
   },
   titleBar: {
     background:
-      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+      "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
   },
   button: {
     margin: 10,
     position: "flex",
-
   },
   button1: {
     margin: 10,
     position: "flex",
-
   },
   size: {
-    width: '100%',
-    height: '150px',
-    display: 'flex',
+    width: "100%",
+    height: "150px",
+    display: "flex",
   },
   center: {
-    position: 'relative',
-    left: '25px',
+    position: "relative",
+    left: "25px",
   },
   loading: {
-    position: 'fixed',
-    top: '10%',
-    width: '100%',
-    height: '30%'
-  }
+    position: "fixed",
+    top: "10%",
+    width: "100%",
+    height: "30%",
+  },
 }));
 const sub = (subtitles) => {
   let subt = {
     ar: null,
     fr: null,
-    en: null
+    en: null,
   };
   if (subtitles) {
     for (var i = 0; i < subtitles.length; i++) {
-      if (subtitles[i].langShort == 'ar')
-        subt.ar = "data:text/vtt;base64," + subtitles[i].fileName
-      if (subtitles[i].langShort == 'fr')
-        subt.fr = "data:text/vtt;base64," + subtitles[i].fileName
-      if (subtitles[i].langShort == 'en')
-        subt.en = "data:text/vtt;base64," + subtitles[i].fileName
+      if (subtitles[i].langShort == "ar")
+        subt.ar = "data:text/vtt;base64," + subtitles[i].fileName;
+      if (subtitles[i].langShort == "fr")
+        subt.fr = "data:text/vtt;base64," + subtitles[i].fileName;
+      if (subtitles[i].langShort == "en")
+        subt.en = "data:text/vtt;base64," + subtitles[i].fileName;
     }
   }
-
   return subt;
-}
+};
 const ViewMovie = (props) => {
-
-  const { user, movieDetails, hash, handleWatch, isOpen, handleClose, similarMovies, handleMovie, comments, handleAddComment, handleChangeComment, handleVp } = props;
+  const {
+    user,
+    movieDetails,
+    hash,
+    handleWatch,
+    isOpen,
+    handleClose,
+    similarMovies,
+    handleMovie,
+    comments,
+    handleAddComment,
+    handleChangeComment,
+    handleVp,
+  } = props;
   const subtitles = sub(movieDetails.subtitles);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const quality = movieDetails.torrents;
   const classes = useStyles();
-
-  const handleClick = event => {
+  const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClosee = () => {
@@ -92,83 +101,86 @@ const ViewMovie = (props) => {
   };
   return (
     <>
-      {movieDetails !== 'loading' && movieDetails !== 'error' &&
-        <div className="root">
-          <Grid container spacing={2}>
-            <Grid item container justify="center" xs={12} sm={4} >
-              {/* {console.log(movieDetails.torrents)} */}
-              {movieDetails.trailer && <Button href={movieDetails.trailer} target="_blank" className={classes.button1} variant="contained" color="primary" startIcon={<YouTubeIcon />}>Trailer</Button>}
-              {movieDetails.torrents && <Button className={classes.button} variant="contained" color="primary" startIcon={<PlayCircleFilledIcon />} onClick={handleClick}>Watch</Button>}
-              {(!movieDetails.torrents  || !movieDetails.trailer ) ? <Typography> Comming soon </Typography> : ''}
-              <img src={movieDetails.Poster} className="image" alt="" />
-              {isOpen && <Modal isOpen={isOpen} handleClose={handleClose}>
-                <video controls width="100%" height="500px">
-                  <source src={"http://localhost:3001/streaming/" + hash} type="video/mp4" />
-                  {subtitles.ar && <track kind="subtitles" src={subtitles.ar} srcLang="ar" default={user.langue === "ar" ? true : false} />}
-                  {subtitles.fr && <track kind="subtitles" src={subtitles.fr} srcLang="fr" default={user.langue === "fr" ? true : false} />}
-                  {subtitles.en && <track kind="subtitles" src={subtitles.en} srcLang="en" default={user.langue === "en" ? true : false} />}
-                </video>
-              </Modal>}
-
-              <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClosee}>
-
-                {quality && Object.keys(quality).map((tile, index) => (
-                  <MenuItem key={index} onClick={() => { handleWatch(tile) }}>{tile}</MenuItem>
-                ))}
-
+    {console.log(movieDetails)}
+      {movieDetails !== "loading" && movieDetails !== "error" && movieDetails.status == "OK" && (
+        <>
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          className="movieContainer"
+        >
+        <Grid item xs={12} className="Cover" ><img src={movieDetails.imgs.fanart} className="imageCover" alt="" /></Grid>
+        <Grid item xs={12} className="CoverShading" ></Grid>
+          <Grid item container className="movieInfosContainer" lg={6} >
+            <Grid
+              item
+              container
+              sm={12}
+              lg={4}
+              alignItems="center"
+            >
+              <Grid item sm={8}>
+                <img src={movieDetails.imgs.poster} className="image" alt="" />
+              </Grid>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClosee}
+              >
+                {quality &&
+                  Object.keys(quality).map((tile, index) => (
+                    <MenuItem
+                      key={index}
+                      onClick={() => {
+                        handleWatch(tile);
+                      }}
+                    >
+                      {tile}
+                    </MenuItem>
+                  ))}
               </Menu>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <List>
-                <Typography color="textSecondary" variant="h2" align="center">{movieDetails.Title}</Typography>
-                <Divider />
-                <ListItem  >
-                  <Typography color="textSecondary" variant="h4" >Description </Typography>
-                </ListItem>
-                <ListItem >
-                  <Typography variant="h6">{movieDetails.Plot}</Typography>
-                </ListItem>
-                <Divider light />
-                <ListItem  >
-                  <Typography color="textSecondary" variant="h4" >Genres </Typography>
-                </ListItem>
-                <ListItem >
-                  <Typography variant="h6">{movieDetails.Genre}</Typography>
-                </ListItem>
-                <Divider light />
-                <ListItem  >
-                  <Typography color="textSecondary" variant="h4" >Year </Typography>
-                </ListItem>
-                <ListItem >
-                  <Typography variant="h5"  >{movieDetails.Year}</Typography>
-                </ListItem>
-                <Divider light />
-                <ListItem  >
-                  <Typography color="textSecondary" variant="h4" >Country </Typography>
-                </ListItem>
-                <Divider light />
-                <ListItem  >
-                  <Typography variant="h5" >{movieDetails.Country} </Typography>
-                </ListItem>
-                <ListItem  >
-                  <Typography color="textSecondary" variant="h4" >Duration </Typography>
-                </ListItem>
-                <Divider light />
-                <ListItem  >
-                  <Typography variant="h5" >{movieDetails.Runtime} </Typography>
-                </ListItem>
-                <ListItem  >
-                  <Typography color="textSecondary" variant="h4" >Actors</Typography>
-                </ListItem>
-                <Divider light />
-                <ListItem  >
-                  <Typography variant="h5" >{movieDetails.Actors} </Typography>
-                </ListItem>
 
-              </List>
+          <Grid item xs={12} sm={12} lg={8}>
+            <Grid item container xs={12} sm={12}>
+              <Grid item xs={9}>
+                <h1 className="movieInfos">
+                  {movieDetails.title} ({movieDetails.year})
+                </h1>
+              </Grid>
+              <Grid item container xs={3} alignItems="center" >
+                <Grid item container xs={6} alignItems="center" style={{ display: "flex", flexDirection: "row"}}><h3 className="movieMiniInfos">{movieDetails.runtime}   </h3><AccessTimeIcon style={{ color: "grey",}}/></Grid>          
+                <Grid item container xs={6} alignItems="center" style={{ display: "flex", flexDirection: "row"}} ><h3 className="movieMiniInfos">{movieDetails.imdb_rating ? Number.parseFloat(movieDetails.imdb_rating).toFixed(1) : ''}  </h3><StarIcon style={{ color: "gold"}}/></Grid>
+              </Grid>
             </Grid>
-            
-            <Grid item xs={12}>
+            <Grid item xs={12} style={{ padding: "10px"}}>
+              <Grid item xs={11}>
+                <h4 className="movieMiniInfos">{movieDetails.description}</h4>
+              </Grid>
+              <Grid item xs={12}>
+                <h2 className="movieInfos">Genre</h2>
+              </Grid>
+              <Grid item xs={12}>
+                <h4 className="movieMiniInfos">{movieDetails.genres}</h4>
+              </Grid>
+              <Grid item xs={12}>
+                <h2 className="movieInfos">Country</h2>
+              </Grid>
+              <Grid item xs={12}>
+                <h4 className="movieMiniInfos">{movieDetails.countries} </h4>
+              </Grid>
+              <Grid item xs={12}>
+                <h2 className="movieInfos">directors</h2>
+              </Grid>
+              <Grid item xs={12}>
+                <h4 className="movieMiniInfos">{movieDetails.directors ? movieDetails.directors : ''}</h4>
+              </Grid>
+            </Grid>
+          </Grid>
+          {/* <Grid item xs={12}>
               {similarMovies && <Typography color="textSecondary" variant="h4" >Similar movies</Typography>}
               {similarMovies && <GridList className={classes.gridList} cols={6}>
                 {similarMovies && similarMovies.map((tile, index) => (
@@ -182,19 +194,34 @@ const ViewMovie = (props) => {
                 ))}
               </GridList>}
             </Grid>
-            <Grid item xs={12}>
-              <Comment handleVp={handleVp} handleChangeComment={handleChangeComment} handleAddComment={handleAddComment} comments={comments} />
-            </Grid>
-          </Grid>
-        </div>
-      }
-
-      {movieDetails === 'loading' &&
+            <Grid item xs={12}> */}
+          {/* <Comment handleVp={handleVp} handleChangeComment={handleChangeComment} handleAddComment={handleAddComment} comments={comments} />
+            </Grid> */}
+        </Grid>
+        </Grid>
+        </>
+      )}
+      {movieDetails === "loading" && (
         <Grid className={classes.loading} container justify="center">
-          <CircularProgress/>
-        </Grid>}
-      {movieDetails === 'error' && Redirect(`http://localhost:3000/`)}
+          <CircularProgress />
+        </Grid>
+      )}
+      {movieDetails === "error" && Redirect(`http://localhost:3000/`)}
     </>
-  )
-}
+  );
+};
 export default ViewMovie;
+{
+  /* {console.log(movieDetails.torrents)} */
+}
+// {movieDetails.trailer && <Button href={movieDetails.trailer} target="_blank" className={classes.button1} variant="contained" color="primary" startIcon={<YouTubeIcon />}>Trailer</Button>}
+// {movieDetails.torrents && <Button className={classes.button} variant="contained" color="primary" startIcon={<PlayCircleFilledIcon />} onClick={handleClick}>Watch</Button>}
+// {(!movieDetails.torrents  || !movieDetails.trailer ) ? <Typography> Comming soon </Typography> : ''}
+// {isOpen && <Modal isOpen={isOpen} handleClose={handleClose}>
+//                 <video controls width="100%" height="500px">
+//                   <source src={"http://localhost:3001/streaming/" + hash} type="video/mp4" />
+//                   {subtitles.ar && <track kind="subtitles" src={subtitles.ar} srcLang="ar" default={user.langue === "ar" ? true : false} />}
+//                   {subtitles.fr && <track kind="subtitles" src={subtitles.fr} srcLang="fr" default={user.langue === "fr" ? true : false} />}
+//                   {subtitles.en && <track kind="subtitles" src={subtitles.en} srcLang="en" default={user.langue === "en" ? true : false} />}
+//                 </video>
+//               </Modal>}
--- server/src/controllers/Library/getMovieData.js
+++ server/src/controllers/Library/getMovieData.js
@@ -1,121 +1,170 @@
-const unirest = require('unirest')
+const unirest = require("unirest");
 // const yifysubtitles = require('yifysubtitles');
-const yifysubtitles = require('@amilajack/yifysubtitles');
-const OS = require('opensubtitles-api');
-const fs = require('fs')
-let cloudscraper = require('cloudscraper');
-const { opensubtitles_config } = require('./opensub');
-
+const yifysubtitles = require("@amilajack/yifysubtitles");
+const OS = require("opensubtitles-api");
+const fs = require("fs");
+let cloudscraper = require("cloudscraper");
+const { opensubtitles_config } = require("./opensub");
 
 getMovieDataById = (id) => {
-    return new Promise((resolve, reject) => {
-        try {
-            let imdb = null;
-            var req = unirest("GET",
-                `https://api.themoviedb.org/3/movie/${id}?api_key=0f87bface5c69fcf394fc387f33049fa&language=en-US`);
-            req.end(function (result) {
-                if (result.error) console.log("timeout in server themoviedb")
-                if (result.body.imdb_id)
-                    imdb = result.body.imdb_id;
-                resolve(imdb);
-            });
-        } catch (error) {
-            console.log('getMovieData timeout')
-            return null;
-        }
-
-    })
-}
+  return new Promise((resolve, reject) => {
+    try {
+      let imdb = null;
+      var req = unirest(
+        "GET",
+        `https://api.themoviedb.org/3/movie/${id}?api_key=0f87bface5c69fcf394fc387f33049fa&language=en-US`
+      );
+      req.end(function (result) {
+        if (result.error) console.log("timeout in server themoviedb");
+        if (result.body.imdb_id) imdb = result.body.imdb_id;
+        resolve(imdb);
+      });
+    } catch (error) {
+      console.log("getMovieData timeout");
+      return null;
+    }
+  });
+};
 getMovieData = async (req, res) => {
-    const data = req.body;
-    const info = {
-        'torrents': "",
-        'trailer': ""
-    }
-    let imdb = data.code;
+  const data = req.body;
+  const info = {
+    torrents: "",
+    trailer: "",
+  };
+  let imdb = data.code;
+//   let imgs = null;
 
-    if (data.type === "id") {
-        let temp = await getMovieDataById(data.code)
-        if (temp !== null)
-            imdb = temp;
-    }
-    // console.log(imdb, 'rrrrrrrrrr')
-    let result1 = await cloudscraper.get(`https://tv-v2.api-fetch.sh/movie/${imdb}`);
-    // let result1 = await axios.get(`http://www.omdbapi.com/?i=${imdb}&apikey=7638ae22`)
-    // .then(response => (
-    //     console.log(response.data)
-    // ))
-    console.log(result1,'nooooooooooo')
-    if (result1) {
-        let x = JSON.parse(result1);
-        info.torrents = x.torrents;
-        info.trailer = x.trailer;
-        console.log(x)
-    }
-    const subtitles = await yifysubtitles(imdb, {path: './subtitles',langs: ['en', 'fr', 'ar']});
+  if (data.type === "id") {
+    let temp = await getMovieDataById(data.code);
+    if (temp !== null) imdb = temp;
+  }
+  // console.log(imdb, 'rrrrrrrrrr')
+  let result1 = await cloudscraper.get(
+    `https://tv-v2.api-fetch.sh/movie/${imdb}`
+  );
+  // let result1 = await axios.get(`http://www.omdbapi.com/?i=${imdb}&apikey=7638ae22`)
+  // .then(response => (
+  //     console.log(response.data)
+  // ))
+  console.log(result1, "nooooooooooo");
+  if (result1) {
+    let x = JSON.parse(result1);
+    info.torrents = x.torrents;
+    info.trailer = x.trailer;
+    console.log(x);
+  }
+  const subtitles = await yifysubtitles(imdb, {
+    path: "./subtitles",
+    langs: ["en", "fr", "ar"],
+  });
 
+  // const OpenSubtitles = new OS(opensubtitles_config);
+  // const subtitles = await OpenSubtitles.search({
+  //     imdbid: imdb,
+  //     lang: ['en', 'fr', 'ar'],
+  //     extensions: ['vtt', 'srt'],
+  // });
+  // console.log(subtitles,'subtitles');
+  // // const lang = Arabic;
+  // if (lang in subtitles) {
+  //     try {
+  //         const dirPath = path.join(__dirname, '..', 'downloads/subtitles', imdb);
+  //         const filePath = path.join(dirPath, `${lang}.vtt`);
+  //         if (!fs.existsSync(dirPath)) {
+  //             const made = await mkdirp(dirPath);
+  //             if (made)
+  //                 fs.writeFile(filePath, '', (err) => {
+  //                     download(subtitles[lang].url)
+  //                         .pipe(srt2vtt())
+  //                         .pipe(fs.createWriteStream(filePath).on('finish', () => console.log('finish')));
+  //                 });
+  //         } else
+  //             fs.writeFile(filePath, '', (err) => {
+  //                 download(subtitles[lang].url)
+  //                     .pipe(srt2vtt())
+  //                     .pipe(fs.createWriteStream(filePath).on('finish', () => console.log('finish')));
+  //             });
+  //     } catch (err) {
+  //         console.log('ma dkhalech');
+  //     }
+  // } else console.log('ma dkhalech');
+  //  const subtitles = await yifysubtitles(imdb, {path: './subtitles',langs: ['en', 'fr', 'ar']});
+  //  for(var i = 0; i < subtitles.length;i++)
+  //   {
+  //     console.log('ah dkhalt')
+  //     const t =  fs.readFileSync('./subtitles/'+subtitles[i].fileName, 'utf8')
+  //     let buff = new Buffer.from(t);
+  //     let base64data = buff.toString('base64');
+  //     subtitles[i].fileName = base64data
+  //   }
 
-    // const OpenSubtitles = new OS(opensubtitles_config);
-    // const subtitles = await OpenSubtitles.search({
-    //     imdbid: imdb,
-    //     lang: ['en', 'fr', 'ar'],
-    //     extensions: ['vtt', 'srt'],
+  // const url = await cloudscraper.get(`http://www.omdbapi.com/?i=${imdb}&apikey=7638ae22`);
+  // console.log(url);
+  // var req = unirest("GET", "https://movies-tvshows-data-imdb.p.rapidapi.com/");
+
+  // req.query({
+  //     i: imdb,
+  //   r: "json"
+  // });
+
+  // req.headers({
+  //     "x-rapidapi-key": "6a4fa63f81msh5cbb2be402fc96bp1eaeb7jsnada33f27f798",
+  //     "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+  //     "useQueryString": true
+  // });
+  var unirest = require("unirest");
+
+  var req = unirest("GET", "https://movies-tvshows-data-imdb.p.rapidapi.com/");
+
+  req.query({
+    type: "get-movie-details",
+    imdb: imdb,
+  });
+
+  req.headers({
+    "x-rapidapi-key": "6a4fa63f81msh5cbb2be402fc96bp1eaeb7jsnada33f27f798",
+    "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+    useQueryString: true,
+  });
+
+    var req1 = unirest("GET", "https://movies-tvshows-data-imdb.p.rapidapi.com/");
+
+    req1.query({
+        "type": "get-movies-images-by-imdb",
+        "imdb": imdb
+    });
+
+    req1.headers({
+        "x-rapidapi-key": "6a4fa63f81msh5cbb2be402fc96bp1eaeb7jsnada33f27f798",
+        "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+        "useQueryString": true
+    });
+
+    // req1.end(function (response) {
+    //     let Data = response.body;
+    //     if (Data) {
+    //         let imgs = Data;
+    //     } else {
+    //         return null;
+    //     }
     // });
-    // console.log(subtitles,'subtitles');
-    // // const lang = Arabic;
-    // if (lang in subtitles) {
-    //     try {
-    //         const dirPath = path.join(__dirname, '..', 'downloads/subtitles', imdb);
-    //         const filePath = path.join(dirPath, `${lang}.vtt`);
-    //         if (!fs.existsSync(dirPath)) {
-    //             const made = await mkdirp(dirPath);
-    //             if (made)
-    //                 fs.writeFile(filePath, '', (err) => {
-    //                     download(subtitles[lang].url)
-    //                         .pipe(srt2vtt())
-    //                         .pipe(fs.createWriteStream(filePath).on('finish', () => console.log('finish')));
-    //                 });
-    //         } else
-    //             fs.writeFile(filePath, '', (err) => {
-    //                 download(subtitles[lang].url)
-    //                     .pipe(srt2vtt())
-    //                     .pipe(fs.createWriteStream(filePath).on('finish', () => console.log('finish')));
-    //             });
-    //     } catch (err) {
-    //         console.log('ma dkhalech');
-    //     } 
-    // } else console.log('ma dkhalech');
-    //  const subtitles = await yifysubtitles(imdb, {path: './subtitles',langs: ['en', 'fr', 'ar']});
-    //  for(var i = 0; i < subtitles.length;i++)
-    //   {
-    //     console.log('ah dkhalt')
-    //     const t =  fs.readFileSync('./subtitles/'+subtitles[i].fileName, 'utf8')
-    //     let buff = new Buffer.from(t);
-    //     let base64data = buff.toString('base64');
-    //     subtitles[i].fileName = base64data
-    //   }
-
-    const url = await cloudscraper.get(`http://www.omdbapi.com/?i=${imdb}&apikey=7638ae22`);
-    console.log(url);
-    // url.query({
-    //     "i": imdb,
-    //     "r": "json"
-    // }),
-    //     url.headers({
-    //         "x-rapidapi-key": "f8042ebf57mshc92a807fbd6224cp1764a0jsnb62c6099b261",
-    //         "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-    //         "useQueryString": true
-    //     }),
-
-    let Data = JSON.parse(url);
+    
+  req.end(function (response) {
+    let Data = response.body;
     if (Data) {
-        Data.torrents = info.torrents.en;
-        console.log(info.trailer, 'okiiiii')
-        Data.trailer = info.trailer;
-        Data.subtitles = subtitles;
+      Data.torrents = info.torrents.en;
+      console.log(info.trailer, "okiiiii");
+      Data.subtitles = subtitles;
+      req1.end(function (response1) {
+        Data.imgs = response1.body;
         res.send({ isData: true, data: Data });
+    });
+    } else {
+      console.log("okiiiii wait tsena");
+      res.send({ isData: false, data: null });
     }
-    else
-        res.send({ isData: false, data: null });
-}
+  });
+
+  
+};
 module.exports = getMovieData;
