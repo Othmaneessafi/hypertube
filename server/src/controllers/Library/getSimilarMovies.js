dce7c380d00e2150cb4f3a6b00c111fe5e85eac8
OthmaneOthy <44867969+OthmaneOthy@users.noreply.github.com>
2021-04-04 19:41:43 +0100

    profile movie almost done

--- server/src/controllers/Library/getSimilarMovies.js
+++ server/src/controllers/Library/getSimilarMovies.js
@@ -4,14 +4,14 @@ const tools = require("../../tools")
 getMovieDataById =  (id) => {
     return new Promise((resolve, reject)=>{
         let imdb = null;
-    // var req =  unirest("GET",
-    // `https://api.themoviedb.org/3/movie/${id}?api_key=0f87bface5c69fcf394fc387f33049fa&language=en-US`);
-    // req.end(function (result) {
-    //     if (result.error) console.log('sss')
-    //     if(result.body.imdb_id)
-    //         imdb = result.body.imdb_id;
-    //     resolve( imdb); 
-    // })
+    var req =  unirest("GET",
+    `https://api.themoviedb.org/3/movie/${id}?api_key=0f87bface5c69fcf394fc387f33049fa&language=en-US`);
+    req.end(function (result) {
+        if (result.error) console.log('sss')
+        if(result.body.imdb_id)
+            imdb = result.body.imdb_id;
+        resolve( imdb); 
+    })
     
 })    
 }
@@ -24,39 +24,39 @@ const getSimilarMovies = async (req,res) =>{
        if(temp !== null)
             imdb = temp;
     }
-    //const imdb = req.body.imdb;
-    //  var req = unirest("GET",
-    //     `http://api.themoviedb.org/3/movie/${imdb}/similar?api_key=0f87bface5c69fcf394fc387f33049fa`);
-    //     req.end(function (result) {
-    //         if (result.error) console.log('result.error')
-    //         let data = result.body;
-    //         if(data)
-    //             res.send(data)
-    //         else
-    //             res.send(null)
-    //     });
-    var req = unirest("GET", "https://movies-tvshows-data-imdb.p.rapidapi.com/");
+    const imdb = req.body.imdb;
+     var req = unirest("GET",
+        `http://api.themoviedb.org/3/movie/${imdb}/similar?api_key=0f87bface5c69fcf394fc387f33049fa`);
+        req.end(function (result) {
+            if (result.error) console.log('result.error')
+            let data = result.body;
+            if(data)
+                res.send(data)
+            else
+                res.send(null)
+        });
+    // var req = unirest("GET", "https://movies-tvshows-data-imdb.p.rapidapi.com/");
 
-    req.query({
-        "type": "get-similar-movies",
-        "imdb": imdb,
-        "page": "1"
-    });
+    // req.query({
+    //     "type": "get-similar-movies",
+    //     "imdb": imdb,
+    //     "page": "1"
+    // });
 
-    req.headers({
-        "x-rapidapi-key": "6a4fa63f81msh5cbb2be402fc96bp1eaeb7jsnada33f27f798",
-        "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
-        "useQueryString": true
-    });
-    req.end(function (result) {
-        if (result.error) console.log('result.error')
-        let data = result.body;
-        console.log(data);
-        if(data)
-            res.send(data)
-        else
-            res.send(null)
-    });
+    // req.headers({
+    //     "x-rapidapi-key": "6a4fa63f81msh5cbb2be402fc96bp1eaeb7jsnada33f27f798",
+    //     "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+    //     "useQueryString": true
+    // });
+    // req.end(function (result) {
+    //     if (result.error) console.log('result.error')
+    //     let data = result.body;
+    //     console.log(data);
+    //     if(data)
+    //         res.send(data)
+    //     else
+    //         res.send(null)
+    // });
 }
 
 module.exports = getSimilarMovies
