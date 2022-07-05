964c505a95a3fc01412c78840b47b7713325997b
ChadiNaour <naourchadi@gmail.com>
2022-07-05 17:31:16 +0100

    sidebar and profile navbar added - still the responsice part and the animation for the sidebar - reload

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -65,49 +65,49 @@ function NavBar(props) {
   return (
     <>
       <div className="absolute top-0 left-24 right-0 h-20 flex justify-end items-center">
-        <div className=" h-full pt-8 pr-10 flex justify-center items-center">
+        <div className=" h-full pt-8 pr-4 flex justify-center items-center">
           <div
-            className="rounded-full bg-[rgba(0,0,0,.2)] mx-2 p-1 pl-6"
+            className="rounded-full bg-[rgba(0,0,0,.2)] mx-2 p-1 pl-4"
           >
             <div className="flex justify-center items-center cursor-pointer ">
               <div className="text-primaryWhite text-2xl" style={{ backgroundColor: "", marginRight: "10px" }}>
                 <div className="cursor-pointer">{user.username}</div>
               </div>
               <div style={{ backgroundColor: "", marginRight: "0px" }}>
-                <img className="flex justify-center items-center h-14 w-14 bg-red-700 rounded-full"
+                <img className="flex justify-center items-center h-12 w-12 bg-red-700 rounded-full"
                   src={`http://localhost:3001/${user.image}`} />
               </div>
             </div>
           </div>
-          <ExitToAppIcon onClick={handleLogout} />
+          {/* <ExitToAppIcon onClick={handleLogout} /> */}
         </div>
       </div>
-      <div className={open ? "fixed top-0 left-0 h-full w-72 bg-black z-40 translate-x-0 ease-in-out duration-300" : "fixed top-0 left-0 h-full w-24 bg-[rgba(0,0,0,.2)] translate ease-in-out duration-300"}>
+      <div className={open ? "fixed top-0 left-0 h-full w-64 bg-black z-40 translate-x-0 ease-in-out duration-300" : "fixed top-0 left-0 h-full w-20 bg-[rgba(0,0,0,.2)] translate ease-in-out duration-300"}>
         <div className="absolute top-0 left-0 w-full h-28 p-1  flex flex-col justify-center items-center">
-          {open ? <img className="h-16" src={Logo} /> : <img src={Hlogo} />}
+          {open ? <img className="h-12" src={Logo} /> : <img src={Hlogo} />}
         </div>
-        <div className={open ? "absolute top-60 left-0 w-full h-auto flex flex-col justify-center items-center" :  "absolute top-1/4 left-0 w-full h-auto flex flex-col justify-center items-center"}>
+        <div className={open ? "absolute top-[20vh] left-0 w-full h-auto flex flex-col justify-center items-center" :  "absolute top-1/4 left-0 w-full h-auto flex flex-col justify-center items-center"}>
           {open ?
           <>
-          <div className="w-full flex flex-row pl-10 p-4 justify-start items-center  text-primaryRed text-2xl"><span className="flex flex-row items-end"><Icon icon="ant-design:home-filled" className="text-primaryRed h-10 w-10 mr-2" />Home</span></div>
-          <div className="w-full  flex flex-row pl-6 p-4 justify-start items-center text-secondary text-2xl"><div className="bg-bg  px-4 w-[95%] py-2 rounded-full flex flex-row"><Icon icon="akar-icons:search" className="text-secondary h-8 w-8 mr-2" />Search</div></div>
-          <div className="w-full flex flex-row pl-10 justify-start items-center text-primaryWhite text-2xl  p-4">Sort by</div>
-          <div className="w-full flex flex-row pl-20 justify-start items-center text-secondary text-xl  p-3"><span className="flex flex-row items-end"><Icon icon="bi:calendar-date-fill" className="text-secondary h-7 w-7 mr-3" />Year</span></div>
-          <div className="w-full flex flex-row pl-20 justify-start items-center text-secondary text-xl  p-3"><span className="flex flex-row items-end"><Icon icon="akar-icons:statistic-up" className="text-secondary h-7 w-7 mr-3" />Popularity</span></div>
-          <div className="w-full flex flex-row pl-20 justify-start items-center text-secondary text-xl  p-3"><span className="flex flex-row items-end"><Icon icon="ant-design:clock-circle-filled" className="text-secondary h-7 w-7 mr-3" />Date added</span></div>
-          <div className="w-full flex flex-row pl-20 justify-start items-center text-secondary text-xl  p-3"><span className="flex flex-row items-end"><Icon icon="ic:baseline-title" className="text-secondary h-7 w-7 mr-3" />Title</span></div>
-          <div className="w-full flex flex-row pl-10 justify-start items-center text-primaryWhite text-2xl  p-4">User</div>
-          <div className="w-full flex flex-row pl-20 justify-start items-center text-secondary text-xl  p-3"><span className="flex flex-row items-end"><Icon icon="ant-design:home-filled" className="text-secondary h-8 w-7 mr-3" />Watchlist</span></div>
+          <div className="w-full flex flex-row pl-10 p-4 justify-start items-center  text-primaryRed text-xl"><span className="flex flex-row items-start"><Icon icon="ant-design:home-filled" className="text-primaryRed h-7 w-7  mr-2" />Home</span></div>
+          <div className="w-full  flex flex-row pl-6 p-4 justify-start items-center text-secondary text-xl"><div className="bg-bg  px-4 w-[90%] py-2 rounded-full flex flex-row items-end"><Icon icon="akar-icons:search" className="text-secondary h-6 w-6 mr-2" />Search</div></div>
+          <div className="w-full flex flex-row pl-10 justify-start items-center text-primaryWhite text-xl  p-2">Sort by</div>
+  7       <div className="w-full flex flex-row pl-20 justify-start items-center text-secondary text-xl  p-3"><span className="flex flex-row items-end"><Icon icon="bi:calendar-date-fill" className="text-secondary h-6 w-6 mr-2" />Year</span></div>
+          <div className="w-full flex flex-row pl-20 justify-start items-center text-secondary text-xl  p-3"><span className="flex flex-row items-end"><Icon icon="akar-icons:statistic-up" className="text-secondary h-6 w-6 mr-2" />Popularity</span></div>
+          <div className="w-full flex flex-row pl-20 justify-start items-center text-secondary text-xl  p-3"><span className="flex flex-row items-end"><Icon icon="ant-design:clock-circle-filled" className="text-secondary h-6 w-6 mr-2" />Date added</span></div>
+          <div className="w-full flex flex-row pl-20 justify-start items-center text-secondary text-xl  p-3"><span className="flex flex-row items-end"><Icon icon="ic:baseline-title" className="text-secondary h-6 w-6 mr-2" />Title</span></div>
+          <div className="w-full flex flex-row pl-10 justify-start items-center text-primaryWhite text-xl  p-2">User</div>
+          <div className="w-full flex flex-row pl-20 justify-start items-center text-secondary text-xl  p-3"><span className="flex flex-row items-end"><Icon icon="ant-design:home-filled" className="text-secondary h-8 w-7 mr-2" />Watchlist</span></div>
           </>
-            : <div className="flex flex-col gap-10"><Icon icon="ant-design:home-filled" className="text-primaryRed h-10 w-10" />
-            <Icon icon="akar-icons:search" className="text-primaryWhite h-8 w-8" />
-            <Icon icon="fa:sort" className="text-primaryWhite h-8 w-8" /></div>}
+            : <div className="flex flex-col gap-10"><Icon icon="ant-design:home-filled" className="text-primaryRed h-8 w-8" />
+            <Icon icon="akar-icons:search" className="text-primaryWhite h-7 w-7" />
+            <Icon icon="fa:sort" className="text-primaryWhite h-7 w-7" /></div>}
         </div>
         <buton className={open ? "absolute cursor-pointer bottom-0 left-0 w-full h-28  flex justify-end p-6 items-center" : "absolute cursor-pointer bottom-0 left-0 w-full h-28  flex justify-center items-center"} onClick={() => {
           setOpen(!open)
         }}>
-          {open ? <Icon icon="ep:d-arrow-left" className="text-primaryWhite h-10 w-10" /> :
-            <Icon icon="ep:d-arrow-right" className="text-primaryWhite h-10 w-10" />
+          {open ? <Icon icon="ep:d-arrow-left" className="text-primaryWhite h-8 w-8" /> :
+            <Icon icon="ep:d-arrow-right" className="text-primaryWhite h-8 w-8" />
           }
         </buton>
         {/* <div></div> */}
